7f64953a600c2402bc4977975899f0219f99a68a
Alexis Lemaire <a.lemaire@klimapartner.de>
2021-01-11 13:06:53 +0100

    Included repository organization check

--- src/main/get-repo-data.js
+++ src/main/get-repo-data.js
@@ -1,13 +1,24 @@
+const { error } = ('@actions/core')
+
 module.exports = () => {
-  console.log('INFO: retrieving repository name and current branch...')
-  const { GITHUB_REPOSITORY, GITHUB_BASE_REF, GITHUB_REF } = process.env
-  const repo = getLastElement(GITHUB_REPOSITORY)
-  const branch = GITHUB_BASE_REF ? getLastElement(GITHUB_BASE_REF) : getLastElement(GITHUB_REF)
-  console.log(`SUCCESS: target repository name is ${repo} and current branch is ${branch}!`)
+  console.log('INFO: retrieving repository and current branch...')
+  const repo = getRepo()
+  const branch = getBranch()
+  console.log(`SUCCESS: target repository is ${repo} and current branch is ${branch}!`)
   return { repo, branch }
 }
 
-// get last element of a slash separated string
-function getLastElement (str) {
-  return str.split('/').pop()
+function getBranch () {
+  const { GITHUB_BASE_REF, GITHUB_REF } = process.env
+  const extractBranch = str => str.split('/').pop()
+  return GITHUB_BASE_REF ? extractBranch(GITHUB_BASE_REF) : extractBranch(GITHUB_REF)
+}
+
+function getRepo () {
+  const repo = process.env.GITHUB_REPOSITORY
+  if (repo.split('/').shift() !== 'kaskadi') {
+    error('Repository has to belong to the kaskadi organization in order to be synchronized.')
+    process.exit(1)
+  }
+  return repo
 }
