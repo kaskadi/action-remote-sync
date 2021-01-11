3e5e6a1b2064b1ac22808ca6d24c429243acf45e
Alexis Lemaire <a.lemaire@klimapartner.de>
2021-01-11 12:44:32 +0100

    Added utility to validate environment variables

--- src/main/main.js
+++ src/main/main.js
@@ -1,9 +1,13 @@
 const { startGroup, endGroup } = require('@actions/core')
 
+const validateEnv = require('./validate-env.js')
 const getRepoData = require('./get-repo-data.js')
 const sync = require('./sync.js')
 
 async function main () {
+  startGroup('Environment variable validation')
+  validateEnv()
+  endGroup()
   startGroup('Repository data retrieval')
   const data = getRepoData()
   endGroup()
